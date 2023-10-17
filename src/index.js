cba99b001ef9657ef7d15db45919b148b42d7f71
ChemicalLuck <j.tebbett@outlook.com>
2023-10-17 10:42:23 +0100

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,105 @@
+class RichText {
+  constructor(input) {
+    this.type = input.type;
+    this.children = (input.children || []).map(child => new Types[child.type](child));
+    this.tag = "div";
+  }
+
+  render() {
+    const container = document.createElement(this.tag);
+    this.children.forEach(child => {
+      container.appendChild(child.render());
+    });
+    return container;
+  }
+}
+
+class Paragraph extends RichText {
+  constructor(input) {
+    super(input);
+    this.tag = "p";
+  }
+}
+
+class Span extends RichText {
+  constructor(input) {
+    super(input);
+    this.tag = "span";
+    this.bold = input.bold || false;
+    this.italic = input.italic || false;
+    this.value = input.value || "";
+  }
+
+  render() {
+    const container = document.createElement(this.tag);
+
+    if (this.bold) {
+      container.style.fontWeight = "bold";
+    }
+    if (this.italic) {
+      container.style.fontStyle = "italic";
+    }
+
+    if (this.value) {
+      container.textContent = this.value;
+    }
+
+    this.children.forEach(child => {
+      container.appendChild(child.render());
+    });
+
+    return container;
+  }
+}
+
+class Link extends RichText {
+  constructor(input) {
+    super(input);
+    this.tag = "a";
+    this.value = input.title;
+    this.url = input.url;
+  }
+
+  render() {
+    const container = document.createElement(this.tag);
+    container.href = this.url;
+    container.textContent = this.value;
+
+    this.children.forEach(child => {
+      container.appendChild(child.render());
+    });
+
+    return container;
+  }
+}
+
+class Heading extends RichText {
+	constructor(input) {
+  	super(input);
+    this.tag = `h${input.level}`
+  }
+}
+
+class List extends RichText {
+		constructor(input) {
+  	super(input);
+    this.tag = `${input.listType[0]}l`
+  }
+}
+
+class ListItem extends RichText {
+		constructor(input) {
+  	super(input);
+    this.tag = "li";
+  }
+}
+
+const Types = {
+  "root": RichText,
+  "paragraph": Paragraph,
+  "text": Span,
+  "link": Link,
+  "heading": Heading,
+  "list": List,
+  "list-item": ListItem
+}
